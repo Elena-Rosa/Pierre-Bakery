0662e19fde0ac6d546583de0aa162a913a93603b
Elena <elenarosamail@gmail.com>
2023-09-30 16:24:36 -0700

    Test 1 Type PASS

--- PierreBakery.Tests/ModelTests/OrderTests.cs
+++ PierreBakery.Tests/ModelTests/OrderTests.cs
@@ -9,20 +9,12 @@ namespace PierreBakery.Tests
   public class OrderTests 
   {
 
-    [TestMethod]
+   [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
       Order newOrder = new Order ("Dry Goods", "Flour", "2024-09-24", 10);
         Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
-      [TestMethod]
-    public void OrderConstructor_SetsPropertiesCorrectly_Order()
-    {
-      Order newOrder = new Order ("Dry Goods", "Flour", "2024-09-24", 10);
-      Assert.AreEqual("Dry Goods", newOrder.Title);
-      Assert.AreEqual("Flour", newOrder.Description);
-      Assert.AreEqual("2024-09-24", newOrder.Date);
-      Assert.AreEqual(10, newOrder.Price);
-    }
   }
 }
+   
--- PierreBakery/Models/Order.cs
+++ PierreBakery/Models/Order.cs
@@ -1,20 +1,42 @@
 using System.Collections.Generic;
 using System;
 
-namespace PierreBakery.Models;
+namespace PierreBakery.Models
+  {
 
   public class Order
   {
+    public int Id { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
     public string Date { get; set; }
     public int Price { get; set; }
+    private static List<Order> _instances = new List<Order> { };
+
   public Order (string title, string description, string date, int price)
   {
     Title = title;
     Description = description;
     Date = date;
     Price = price;
+     _instances.Add(this);
+      Id = _instances.Count;
   }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
   }
+}
   
