fbfb394b29cc16ede94c028850b81e647942d303
Elena <elenarosamail@gmail.com>
2023-10-01 07:32:54 -0700

    Vendor TEST 1,2 PASS

--- PierreBakery/Models/Vendor.cs
+++ PierreBakery/Models/Vendor.cs
@@ -1,23 +1,42 @@
 using System.Collections.Generic;
 using System;
 
-namespace PierreBakery.Models;
-
+namespace PierreBakery.Models
+  {
   public class Vendor
   
   {
+    private static List<Vendor> _instances = new List<Vendor> {};
     public string Name { get; set; }
+    public int Id { get; }
     public string Description { get; set; }
-    public static List <Order> Orders { get; set; }
+    public List <Order> Orders { get; set; }
+
    public Vendor (string name, string description)
    {
      Name = name;
      Description = description;
      Orders = new List<Order> {};
+      _instances.Add(this);
+      Id = _instances.Count;
    }
-   public static List<Order> GetAll()
-   {
-     return Orders;
-   }
-   
+
+    public static List<Vendor> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+    public static Vendor Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+    public void AddOrder(Order order)
+    {
+      Orders.Add(order);
+    }
   }
+}
